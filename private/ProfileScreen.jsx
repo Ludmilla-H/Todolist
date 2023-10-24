c65fac91c1a597ef5639d3986c064b8cbc54dac6
Ludmilla-H <ludmillaharnais31@gmail.com>
2023-10-24 10:52:21 -0400

    profil page

--- private/ProfileScreen.jsx
+++ private/ProfileScreen.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator, Platform } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Button, TextInput } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import Icon from 'react-native-vector-icons/dist/EvilIcons'
+import storage from '@react-native-firebase/storage';
+import ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
 
 
 
@@ -16,12 +20,41 @@ const ProfileScreen = () => {
   const [nom, setNom] = useState('');
   const [prenom, setPrenom] = useState('');
   const [email, setEmail] = useState('');
-  const [user, setUser] = useState();
+  // const [user, setUser] = useState();
+  const [status, setStatus] = useState();
 
 
-  const addImage = () => {
-
+  const chooseFile = async () => {
+    const options = {
+      title: 'Select Image',
+      storageOptions: {
+          skipBackup: true, // do not backup to iCloud
+          path: 'images', // store camera images under Pictures/images for android and Documents/images for iOS
+      },
   }
+  const result = await launchCamera(options , callBackCamera())
+  console.log("result" , result)
+
+};
+
+const callBackCamera = (response) => {
+  console.log(response)
+}
+
+// ImagePicker.showImagePicker(options, response => {
+//   if (response.didCancel) {
+//       console.log('User cancelled image picker', storage());
+//   } else if (response.error) {
+//       console.log('ImagePicker Error: ', response.error);
+//   } else if (response.customButton) {
+//       console.log('User tapped custom button: ', response.customButton);
+//   } else {
+//     console.log(response)
+//       // let path = this.getPlatformPath(response).value;
+//   }
+// });
+
+  
 
   const modifier = () => {
 
@@ -70,7 +103,7 @@ const ProfileScreen = () => {
         onChangeText={setEmail}
       />
 
-      <Button onPress={addImage} > + </Button>
+      <Button onPress={chooseFile} > + </Button>
       <Button onPress={modifier} > Valider les modifications</Button>
       <Button onPress={deconexion}>Déconexion</Button>
 
